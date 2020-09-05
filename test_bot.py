0ba89e71856b3ce6019cf1425b9b678da7f9f8b1
e-seng <eth.sengsavang@gmail.com>
2020-09-05 15:37:47 -0600

    Add additional documentation

--- test_bot.py
+++ test_bot.py
@@ -13,6 +13,10 @@ def init_stock_data(stock_ticker, rootdir="."):
     Initializes the stock data csv for a given stock.
     This will produce a csv the appropreate headers for the stock history.
 
+    This file will be saved within, unless otherwise specified, the root
+    directory in the data folder. Filenames will follow the format of
+    sd-<stock ticker name, in lower case>.csv
+
     @params 
       - stock_ticker {str} The ticker for the desired stock
       - filepath? {str} The filepath to store this data, defaults to the

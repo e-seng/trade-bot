71bf533915bbd61bbe16d0d41c3ca6184eba38d8
e-seng <eth.sengsavang@gmail.com>
2020-09-05 14:47:25 -0600

    Create init stock data function

--- test_bot.py
+++ test_bot.py
@@ -6,6 +6,18 @@ import numpy as np
 
 from sys import argv
 
+def init_stock_data(stock_ticker, rootdir="."):
+    """
+    Initializes the stock data csv for a given stock.
+    This will produce a csv the appropreate headers for the stock history.
+
+    @params 
+      - stock_ticker {str} The ticker for the desired stock
+      - filepath? {str} The filepath to store this data, defaults to the
+        root directory
+    """
+    
+
 def main():
     tsla = yf.Ticker("TSLA")
     print(tsla.info)

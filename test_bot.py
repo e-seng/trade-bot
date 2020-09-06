81268151b114265999d6378cf8dd1605cf429b32
e-seng <eth.sengsavang@gmail.com>
2020-09-05 22:45:28 -0600

    Update documentation

--- test_bot.py
+++ test_bot.py
@@ -18,26 +18,26 @@ def init_stock_data(stock_ticker, rootdir="."):
     sd-<stock ticker name, in lower case>.csv
 
     The csv will contain the following headers:
-    prev_changed
-      - The amount has risen or has fallen for the stock
-    avg_drop
-      - The average amount the value has dropped when the value has historically
+      - prev_changed:
+        The amount has risen or has fallen for the stock
+      - avg_drop:
+        The average amount the value has dropped when the value has historically
         changed by prev_changed (0 if non-existant)
-    avg_rise
-      - The average amount the value has risen when the value has historically
+      - avg_rise:
+        The average amount the value has risen when the value has historically
         changed by prev_changed (0 if non-existant)
-    max_drop
-      - The maximum amount the value has dropped when the value has historically
+      - max_drop:
+        The maximum amount the value has dropped when the value has historically
         changed by prev_changed (0 if non-existant)
-    min_rise
-      - The maximum amoun the value has risen by when the value has historically
+      - min_rise:
+        The maximum amoun the value has risen by when the value has historically
         changed by prev_changed (0 if non-existant)
 
     @params 
-    stock_ticker {str}
-      - The ticker for the desired stock
-    filepath? {str}
-      - The filepath to store this data, defaults to the current directory
+      - stock_ticker {str}:
+        The ticker for the desired stock
+      - filepath? {str}:
+        The filepath to store this data, defaults to the current directory
     """
     filename = f"sd-{stock_ticker.lower()}.csv"
     folderpath = os.path.join(rootdir, "data")
